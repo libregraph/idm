29641a3ec70fcc32ae9723af64d6ee1188f5952b
Simon Eisenmann <s.eisenmann@kopano.com>
2021-04-21 15:16:01 +0200

    Use template syntax in demo users generator

--- scripts/generate-demo-users-ldif.sh
+++ scripts/generate-demo-users-ldif.sh
@@ -4,5 +4,4 @@ set -e
 
 CURL=curl
 
-$CURL -qs https://raw.githubusercontent.com/kopano-dev/kopano-docker/master/ldap_demo/bootstrap/ldif/demo-users.ldif | sed "s;{{ LDAP_DOMAIN }};$(hostname -f);g" | sed "s;{{ LDAP_BASE_DN }};dc=nodomain;g"
-
+exec $CURL -qs https://raw.githubusercontent.com/kopano-dev/kopano-docker/master/ldap_demo/bootstrap/ldif/demo-users.ldif | sed "s;{{ LDAP_DOMAIN }};{{.MailDomain}};g" | sed "s;{{ LDAP_BASE_DN }};{{.BaseDN}};g"
