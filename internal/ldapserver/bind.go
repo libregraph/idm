42e56d1afcafde8a81399ee9a8526edc5c137a6f
Simon Eisenmann <s.eisenmann@kopano.com>
2021-04-26 11:39:51 +0200

    Add proper license headers and origin reference

--- internal/ldapserver/bind.go
+++ internal/ldapserver/bind.go
@@ -1,3 +1,8 @@
+// Copyright 2012 The Go Authors. All rights reserved.
+// Copyright 2021 Kopano and its licensors.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package ldapserver
 
 import (
--- internal/ldapserver/stats.go
+++ internal/ldapserver/stats.go
@@ -1,7 +1,7 @@
-/*
- * SPDX-License-Identifier: AGPL-3.0-or-later
- * Copyright 2021 Kopano and its licensors
- */
+// Copyright 2012 The Go Authors. All rights reserved.
+// Copyright 2021 Kopano and its licensors.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 package ldapserver
 
